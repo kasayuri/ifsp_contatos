532e4b6e281ca3af9adb9bcce71ee73a7f548064
Karina <pste09.informatica@crecisp.gov.br>
2025-09-25 13:07:47 -0300

    desenvolvimento do adapter que vai realizar a ligação dos dados com a lista de contatos, no vídeo abaixo é realizado a continuação do projeto.
    https://youtu.be/uyqzMLzyKFQ

--- app/src/main/java/com/example/contatos/MainActivity.java
+++ app/src/main/java/com/example/contatos/MainActivity.java
@@ -34,8 +34,15 @@ public class MainActivity extends AppCompatActivity {
 
     private void loadContatos(){
         ContatoDAO contatoDAO = new ContatoDAO(this);
-        //List<Contato> contatos = contatoDAO.listarContatos();
-        List<Contato> contatos = fakeContatos();
+
+        Contato c1 = new Contato(1, "Contato 01");
+        Contato c2 = new Contato(2, "Contato 02");
+        Contato c3 = new Contato(3, "Contato 03");
+        contatoDAO.inserirContato(c1);
+        contatoDAO.inserirContato(c2);
+        contatoDAO.inserirContato(c3);
+
+        List<Contato> contatos = contatoDAO.listarContatos();
 
         ContatoAdapter contatoAdapter = new ContatoAdapter(contatos, this);
 
@@ -43,16 +50,4 @@ public class MainActivity extends AppCompatActivity {
         lvContatos.setAdapter(contatoAdapter);
     }
 
-    private List<Contato> fakeContatos() {
-        Contato c1 = new Contato(1, "Contato 01");
-        Contato c2 = new Contato(2, "Contato 02");
-        Contato c3 = new Contato(3, "Contato 03");
-
-        List<Contato> contatos = new ArrayList<>();
-        contatos.add(c1);
-        contatos.add(c2);
-        contatos.add(c3);
-
-        return contatos;
-    }
 }
